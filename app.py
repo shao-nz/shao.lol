99b8c4b25225e1b3e70d9bd7c646db5f16087ea1
esha7130 <esha7130@uni.sydney.edu.au>
2021-12-01 03:40:33 +1100

    remove old functions

--- app.py
+++ app.py
@@ -8,26 +8,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/projects")
-def projects():
-    return render_template("projects.html")
-
-@app.route("/ipt")
-def ipt():
-    return render_template("ipt.html")
-
-@app.route("/commsys")
-def commsys():
-    return render_template("commsys.html")
-
-@app.route ("/ccommsys")
-def ccommsys():
-    return render_template("ccommsys.html")
-
-@app.route ("/trcommsys")
-def trcommsys():
-    return render_template("trcommsys.html")
-
 
 if __name__ == "__main_":
     app.run(host="0.0.0.0", port=80)
