b3f621bf8974a513a60fac5445add0d3d96149c6
esha7130 <esha7130@uni.sydney.edu.au>
2021-12-01 17:52:23 +1100

    fix views

--- app/views.py
+++ app/views.py
@@ -1,10 +1,9 @@
-from flask.templating import render_template
+from flask import render_template
 from app import app
 
 @app.route("/")
 def home():
-    # return render_template("index.html")
-    return "this is the index page"
+    return "Hello world!"
 
 @app.route("/template")
 def template():
