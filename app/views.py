b30c355c784a257c2c3ac52ed647956bf83f06f8
esha7130 <esha7130@uni.sydney.edu.au>
2021-12-01 17:32:25 +1100

    add new route for index

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
+from flask.templating import render_template
 from app import app
 
 @app.route("/")
 def home():
     # return render_template("index.html")
     return "this is the index page"
+
+@app.route("/template")
+def template():
+    return render_template("index.html")
