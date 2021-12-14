c2aa71c48769804edf25bdc3d1bfebf7967cd292
esha7130 <esha7130@uni.sydney.edu.au>
2021-12-15 04:14:30 +1100

    change backend from Flask to node.js

---
+++ app/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './static/cursed_flushed.png';
+import './styles/App.css';
+import { Tracker } from './Tracker'
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className='App'>
+          <h1>shao.lol tracker</h1>
+          <img src={logo} />
+          <Tracker />
+      </div>
+    );
+  }
+}
+
+export default App;
