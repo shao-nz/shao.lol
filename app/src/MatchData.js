1643638fdeb4d09dd0d66a36ba7ae73253c10862
esha7130 <esha7130@uni.sydney.edu.au>
2021-12-26 03:27:05 +1100

    add display for champion level, cs stats, game duration

--- app/src/MatchData.js
+++ app/src/MatchData.js
@@ -30,6 +30,12 @@ export class MatchData extends React.Component {
     this.setState({
       loaded: true
     })
+    // console.log(this.state.matchDetails)
+    // var thing = this.state.matchDetails.sort(function(a, b) {
+    //   // console.log(b.info.gameCreation - a.info.gameCreation)
+    //   return a.info.gameCreation - b.info.gameCreation;
+    // })
+    // console.log(thing)
   }
 
   epochConverter = (time) => {
@@ -63,7 +69,6 @@ export class MatchData extends React.Component {
       this.state.loaded
       &&
       this.state.matchDetails.map((match) => {
-
           return (
           <DisplayMatchData
               matchData={match}
@@ -78,7 +83,7 @@ export class MatchData extends React.Component {
 
 const DisplayMatchData = ({matchData, puuid}) => {
   var background = '';
-  
+
   var win = matchData.info.participants.find(participants => participants.puuid == puuid).win;
   var teamEarlySurrendered = matchData.info.participants.find(participants => participants.puuid == puuid).teamEarlySurrendered;
   win ? background = 'green' : background = 'red'
@@ -87,10 +92,23 @@ const DisplayMatchData = ({matchData, puuid}) => {
   }
   return (
     <div className='individualGame' style={{ backgroundColor: background}}>
+    {/* {matchData.info.gameCreation} <br />
+    {matchData.info.gameStartTimestamp} <br />
+    {matchData.info.gameId} */}
+    <DisplayChampLevel
+      champLevel={matchData.info.participants.find(participants => participants.puuid == puuid).champLevel}
+    />
+    <DisplayGameDuration
+      gameDuration={matchData.info.gameDuration}
+    />
     <DisplayKDA
       kills={matchData.info.participants.find(participants => participants.puuid == puuid).kills}
       deaths={matchData.info.participants.find(participants => participants.puuid == puuid).deaths}
       assists={matchData.info.participants.find(participants => participants.puuid == puuid).assists}
+    />
+    <DisplayCSStats
+      cs={matchData.info.participants.find(participants => participants.puuid == puuid).totalMinionsKilled}
+      gameDuration={matchData.info.gameDuration}
     />
     <DisplaySummonerNames
       summonerList={matchData.info.participants}
@@ -100,10 +118,26 @@ const DisplayMatchData = ({matchData, puuid}) => {
   )
 }
 
+class DisplayGameDuration extends React.Component {
+  secondsToMMSS = (seconds) => {
+    var timeString = '';
+    var MM = Math.floor(seconds/60);
+    var SS = seconds - (MM * 60);
+    timeString = `${MM}:${SS.toString().padStart(2, "0")}`;
 
+    return timeString;
+  }
+
+  render() {
+    return (
+      <div className='gameDuration'>
+        {this.secondsToMMSS(this.props.gameDuration)}
+      </div>
+    )
+  }
+}
 
 class DisplaySummonerNames extends React.Component {
-
   getSummonerNamesByTeam = (summonerList) => {
     var summonersByTeamDict = {};
     for (const summonerDict of summonerList) {
@@ -133,6 +167,19 @@ class DisplaySummonerNames extends React.Component {
   }
 }
 
+const DisplayChampLevel = ({champLevel}) => (
+  <div className='champLevel'>
+    Level {champLevel}
+  </div>
+)
+
+const DisplayCSStats = ({cs, gameDuration}) => (
+  <div className='CSStats'>
+    {cs} CS <br />
+    <b>({(cs/(gameDuration/60)).toFixed(1)})</b> CS/m
+  </div>
+)
+
 
 const DisplayKDA = ({kills, deaths, assists}) => (
   <div className='KDA'>
@@ -147,8 +194,8 @@ const DisplayBlueTeam = (blueTeamList) => (
       {
         blueTeamList.blueTeamList.map((summoner) => {
           return (
-            <div className='summoner'>
-              <li key={summoner.summonerName}>
+            <div className='summoner' key={summoner.summonerName}>
+              <li>
               <img src={'https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/' + summoner.championId + '.png'}/> {summoner.summonerName}
               </li>
             </div>
@@ -166,8 +213,8 @@ const DisplayRedTeam = (redTeamList) => (
       {
         redTeamList.redTeamList.map((summoner) => {
           return (
-            <div className='summoner'>
-              <li key={summoner.summonerName}>
+            <div className='summoner' key={summoner.summonerName}>
+              <li>
               <img src={'https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/' + summoner.championId + '.png'}/> {summoner.summonerName}
               </li>
             </div>
